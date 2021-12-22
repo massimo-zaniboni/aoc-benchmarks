696583e934fb8ef457377255be53c3be1eebf7b7
Massimo Zaniboni <mzan@dokmelody.org>
2021-12-22 18:32:14 +0100

    Bug fix and improvements

--- benchmarks/bencher/programs/aoc2021_day06b/aoc2021_day06b.clj-1.clj
+++ benchmarks/bencher/programs/aoc2021_day06b/aoc2021_day06b.clj-1.clj
@@ -1,5 +1,5 @@
 ; SPDX-License-Identifier: MIT
-; Copyright (C) 2021 Tito Sacchi &lt;tito&#64;tilde.team&gt;
+; Copyright (C) 2021 Tito Sacchi <tito@tilde.team>
 ; WARNING: These solutions were written while I was still learning Clojure and
 ; should by no means be taken as examples of good programming practice or fast
 ; implementations.
@@ -9,7 +9,7 @@
     [clojure.string :as str]))
 
 (defn parse [line]
-  (vec (map #(Integer/parseInt %) (str/split line #&quot;,&quot;))))
+  (vec (map #(Integer/parseInt %) (str/split line #","))))
 
 (defn prdbg [x] (do (println x) x))
 (defn ifnil [x y] (if (nil? x) y x))
@@ -22,16 +22,15 @@
             timers (reduce-kv #(assoc %1 (dec %2) %3) {} freqs)
             new (get timers -1)]
         (recur (inc k) (if (some? new)
-                         (-&gt; timers
+                         (-> timers
                              (update 8 #(+ (ifnil % 0) new))
                              (update 6 #(+ (ifnil % 0) new))
                              (dissoc -1))
                          timers))))))
 
-(def n 256)
-(println (-&gt;&gt; (read-line)
-              (parse)
-              (frequencies)
-              (step n)
-              (vals)
-              (apply +)))
+(let [freqs (->> (read-line) (parse) (frequencies))
+      n (->> (read-line) (Integer/parseInt))]
+
+     (println (-> (step n freqs)
+                  (vals)
+                  (apply +))))
